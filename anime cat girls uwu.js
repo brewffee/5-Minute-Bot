f8970b78045a82fdf588a76bb4f8de49cbb01f9b
geixco <68980781+geixco@users.noreply.github.com>
2020-12-23 10:46:27 -0500

    pogU

---
+++ anime cat girls uwu.js
@@ -0,0 +1,15 @@
+const d=new (require('discord.js')).Client(),
+cf=require('./config.json');
+d.cs=new (require('discord.js')).Collection();
+(require('fs')).readdir('./commands/',(e,c)=>{
+    if(e)return console.log('err loading');
+    c.forEach(c=> d.cs.set(c.split('.')[0], require(`./commands/${c}`)));
+});
+d.on('ready',()=> console.log('ready'))
+    .on('message',m=>{
+        const a=m.content.slice(cf.p.length).trim().split(/ +/g),
+        cm=d.cs.get(a.shift().toLowerCase());
+        if(m.author.bot||!m.content.indexOf(cf.p)===0||!cm)return;
+        command.r(m, args);
+    });
+d.login(cf.t);
